570a475300cae45e30e6929d3dde97dbf3181884
Abhraneel-11-06 <abhraneel.karmakar.22@aot.edu.in>
2023-08-28 01:28:59 +0530

    Fixed bugs in homepage

--- js/product.js
+++ js/product.js
@@ -1,104 +1,104 @@
-// /* ========== Products Slider =========== */
-// const swiper = new Swiper('.mySwiper', {
-//   grabCursor: true,
-//   slidesPerView: 1,
-//   spaceBetween: 70,
-//   pagination: {
-//     el: '.custom-pagination',
-//     clickable: true,
-//   },
-//   breakpoints: {
-//     567: {
-//       slidesPerView: 2,
-//     },
-//     996: {
-//       slidesPerView: 3,
-//     },
-//   },
-// });
+/* ========== Products Slider =========== */
+const swiper = new Swiper('.mySwiper', {
+  grabCursor: true,
+  slidesPerView: 1,
+  spaceBetween: 70,
+  pagination: {
+    el: '.custom-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    567: {
+      slidesPerView: 2,
+    },
+    996: {
+      slidesPerView: 3,
+    },
+  },
+});
 
 // /* ========== Fetch the Products =========== */
 
-// const getProducts = async () => {
-//   try {
-//     const results = await fetch('./data/products.json');
-//     const data = await results.json();
-//     const products = data.products;
-//     return products;
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
+const getProducts = async () => {
+  try {
+    const results = await fetch('./data/products.json');
+    const data = await results.json();
+    const products = data.products;
+    return products;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-// const ProductsWrapper = document.getElementById('products');
+const ProductsWrapper = document.getElementById('products');
 
-// window.addEventListener('DOMContentLoaded', async function () {
-//   let products = await getProducts();
-//   products = products.filter((product) => product.category === 'Tshirt');
-//   displayProductItems(products);
-//   loadData();
-// });
+window.addEventListener('DOMContentLoaded', async function () {
+  let products = await getProducts();
+  products = products.filter((product) => product.category === 'Tshirt');
+  displayProductItems(products);
+  loadData();
+});
 
-// /* ========== Display Products =========== */
-// const displayProductItems = (items) => {
-//   let displayProduct = items.map(
-//     (product) => `
-//                 <div class="swiper-slide">
-//               <div class="product">
-//                 <div class="top d-flex">
-//                   <img src=${product.url} alt="" />
-//                   <div class="icon d-flex">
-//                     <i class="bx bxs-heart"></i>
-//                   </div>
-//                 </div>
-//                 <div class="bottom">
-//                   <h4>${product.title}</h4>
-//                   <div class="d-flex">
-//                     <div class="price">&#8377;${product.price}</div>
-//                     <div class="rating">
-//                       <i class="bx bxs-star"></i>
-//                       <i class="bx bxs-star"></i>
-//                       <i class="bx bxs-star"></i>
-//                       <i class="bx bxs-star"></i>
-//                       <i class="bx bxs-star"></i>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//                   `
-//   );
+/* ========== Display Products =========== */
+const displayProductItems = (items) => {
+  let displayProduct = items.map(
+    (product) => `
+                <div class="swiper-slide">
+              <div class="product">
+                <div class="top d-flex">
+                  <img src=${product.url} alt="" />
+                  <div class="icon d-flex">
+                    <i class="bx bxs-heart"></i>
+                  </div>
+                </div>
+                <div class="bottom">
+                  <h4>${product.title}</h4>
+                  <div class="d-flex">
+                    <div class="price">&#8377;${product.price}</div>
+                    <div class="rating">
+                      <i class="bx bxs-star"></i>
+                      <i class="bx bxs-star"></i>
+                      <i class="bx bxs-star"></i>
+                      <i class="bx bxs-star"></i>
+                      <i class="bx bxs-star"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+                  `
+  );
 
-//   displayProduct = displayProduct.join('');
-//   ProductsWrapper.innerHTML = displayProduct;
-// };
+  displayProduct = displayProduct.join('');
+  ProductsWrapper.innerHTML = displayProduct;
+};
 
-// /* ========== Filter Products =========== */
-// const filters = [...document.querySelectorAll('.filters div')];
+/* ========== Filter Products =========== */
+const filters = [...document.querySelectorAll('.filters div')];
 
-// filters.forEach((filter) => {
-//   filters[2].classList.add('active');
-//   filter.addEventListener('click', async (e) => {
-//     const id = e.target.getAttribute('data-filter');
-//     const target = e.target;
-//     const products = await getProducts();
-//     filters.forEach((btn) => {
-//       btn.classList.remove('active');
-//     });
-//     target.classList.add('active');
+filters.forEach((filter) => {
+  filters[2].classList.add('active');
+  filter.addEventListener('click', async (e) => {
+    const id = e.target.getAttribute('data-filter');
+    const target = e.target;
+    const products = await getProducts();
+    filters.forEach((btn) => {
+      btn.classList.remove('active');
+    });
+    target.classList.add('active');
 
-//     let menuCategory = products.filter((product) => {
-//       if (product.category === id) {
-//         return product;
-//       }
-//     });
+    let menuCategory = products.filter((product) => {
+      if (product.category === id) {
+        return product;
+      }
+    });
 
-//     displayProductItems(menuCategory);
-//     swiper.update();
-//   });
-// });
+    displayProductItems(menuCategory);
+    swiper.update();
+  });
+});
 
-// /* ========== Categories Products =========== */
+/* ========== Categories Products =========== */
 
 // const categoriesProducts = document.querySelector('.categories .products');
 // const loadmore = document.querySelector('.loadmore');
@@ -146,5 +146,5 @@
 //   currentIndex += maxResult;
 // }
 
-// loadmore.addEventListener('click', loadData);
+loadmore.addEventListener('click', loadData);
 console.log("");
