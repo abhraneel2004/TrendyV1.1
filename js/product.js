728808245730cd6df83897ac89fd78cabfc4f8b2
Abhraneel-11-06 <abhraneel.karmakar.22@aot.edu.in>
2023-08-28 10:53:20 +0530

    Bug fixing in login page

--- js/product.js
+++ js/product.js
@@ -100,51 +100,51 @@ filters.forEach((filter) => {
 
 /* ========== Categories Products =========== */
 
-// const categoriesProducts = document.querySelector('.categories .products');
-// const loadmore = document.querySelector('.loadmore');
+const categoriesProducts = document.querySelector('.categories .products');
+const loadmore = document.querySelector('.loadmore');
 
-// let currentIndex = 0;
-// async function loadData() {
-//   let maxResult = 8;
-//   let products = await getProducts();
-//   if (currentIndex >= products.length) {
-//     loadmore.disabled = true;
-//     loadmore.innerText = 'No More Products';
-//     return;
-//   }
+let currentIndex = 0;
+async function loadData() {
+  let maxResult = 8;
+  let products = await getProducts();
+  if (currentIndex >= products.length) {
+    loadmore.disabled = true;
+    loadmore.innerText = 'No More Products';
+    return;
+  }
 
-//   for (var i = 0; i < maxResult; i++) {
-//     const product = products[i + currentIndex];
-//     categoriesProducts.insertAdjacentHTML(
-//       'beforeend',
-//       `<div class="product">
-//           <div class="top d-flex">
-//             <img src=${product.url} alt="" />
-//             <div class="icon d-flex">
-//               <i class="bx bxs-heart"></i>
-//             </div>
-//           </div>
-//           <div class="bottom">
-//             <div class="d-flex">
-//               <h4>${product.title}</h4>
-//             </div>
-//             <div class="d-flex">
-//               <div class="price">&#8377;${product.price}</div>
-//               <div class="rating">
-//                 <i class="bx bxs-star"></i>
-//                 <i class="bx bxs-star"></i>
-//                 <i class="bx bxs-star"></i>
-//                 <i class="bx bxs-star"></i>
-//                 <i class="bx bxs-star"></i>
-//               </div>
-//             </div>
-//           </div>
-//         </div>`
-//     );
-//   }
+  for (var i = 0; i < maxResult; i++) {
+    const product = products[i + currentIndex];
+    // categoriesProducts.insertAdjacentHTML(
+    //   'beforeend',
+    //   `<div class="product">
+    //       <div class="top d-flex">
+    //         <img src=${product.url} alt="" />
+    //         <div class="icon d-flex">
+    //           <i class="bx bxs-heart"></i>
+    //         </div>
+    //       </div>
+    //       <div class="bottom">
+    //         <div class="d-flex">
+    //           <h4>${product.title}</h4>
+    //         </div>
+    //         <div class="d-flex">
+    //           <div class="price">&#8377;${product.price}</div>
+    //           <div class="rating">
+    //             <i class="bx bxs-star"></i>
+    //             <i class="bx bxs-star"></i>
+    //             <i class="bx bxs-star"></i>
+    //             <i class="bx bxs-star"></i>
+    //             <i class="bx bxs-star"></i>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>`
+    // );
+  }
 
-//   currentIndex += maxResult;
-// }
+  currentIndex += maxResult;
+}
 
 // loadmore.addEventListener('click', loadData);
 console.log("");
